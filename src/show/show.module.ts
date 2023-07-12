aa90b0866dec15b11b009bd673262334897962cd
beedyboy <boladebode@gmail.com>
2023-07-12 19:24:06 +0100

    chore: create controller

--- src/show/show.module.ts
+++ src/show/show.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { ShowService } from './show.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Show } from 'src/entities/show.entity';
+import { ShowController } from './show.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Show])],
   providers: [ShowService],
+  controllers: [ShowController],
 })
 export class ShowModule {}
