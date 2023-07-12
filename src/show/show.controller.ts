ebe13c9cfc2d1157efb1c2774cadd2f4d3bc4a06
beedyboy <boladebode@gmail.com>
2023-07-12 19:39:10 +0100

    chore: pass test for buyItem

--- src/show/show.controller.ts
+++ src/show/show.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Param, NotFoundException } from '@nestjs/common';
+import { ShowService } from './show.service';
+import { Show } from '../entities/show.entity';
 
 @Controller('show')
-export class ShowController {}
+export class ShowController {
+  constructor(private readonly showService: ShowService) {}
+
+  @Post(':showID/buy_item/:itemID')
+  async buyItem(
+    @Param('showID') showID: number,
+    @Param('itemID') itemID: number,
+  ): Promise<Show> {
+    const show = await this.showService.buyItem(showID, itemID);
+
+    if (!show) {
+      throw new NotFoundException('Show or inventory item not found');
+    }
+
+    return show;
+  }
+}
