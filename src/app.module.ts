528b4cd4ccb84b10cda1002f5dab5bb5b62341f1
beedyboy <boladebode@gmail.com>
2023-07-12 06:35:15 +0100

    chore: setup database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DATABASE_NAME,
+      synchronize: process.env.SYNCHRONIZE ? true : false,
+      entities: ['dist/**/*.entity.js'],
+    }),
+  ],
 })
 export class AppModule {}
