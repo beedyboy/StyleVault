866510f4e77dbf4fa61826920cb3a33a5587683e
beedyboy <boladebode@gmail.com>
2023-07-12 13:09:03 +0100

    chore: write controller logic to create  inventory

--- src/inventory/inventory.controller.ts
+++ src/inventory/inventory.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpStatus, Post } from '@nestjs/common';
+import { InventoryService } from './inventory.service';
+import { InventoryItemDto } from 'src/dtos/inventory.dto';
 
 @Controller('inventory')
-export class InventoryController {}
+export class InventoryController {
+  constructor(private readonly inventoryService: InventoryService) {}
+
+  @Post('create')
+  async createInventory(
+    @Body() inventoryItems: InventoryItemDto[],
+  ): Promise<{ message: string; status: number }> {
+    try {
+      await Promise.all(
+        inventoryItems.map((item) =>
+          this.inventoryService.createInventory(item),
+        ),
+      );
+      return {
+        message: 'Inventory created successfully',
+        status: HttpStatus.CREATED,
+      };
+    } catch (error) {
+      return {
+        message: error.message || 'Failed to create or update inventory',
+        status: HttpStatus.INTERNAL_SERVER_ERROR,
+      };
+    }
+  }
+}
