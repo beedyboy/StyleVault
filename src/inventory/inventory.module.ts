9627fe258a4cc3c441b0966130516f7f44edf231
beedyboy <boladebode@gmail.com>
2023-07-12 10:05:43 +0100

    chore: create  inventory controller

--- src/inventory/inventory.module.ts
+++ src/inventory/inventory.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { InventoryService } from './inventory.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Inventory } from './inventory.entity';
+import { InventoryController } from './inventory.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Inventory])],
   providers: [InventoryService],
+  controllers: [InventoryController],
 })
 export class InventoryModule {}
