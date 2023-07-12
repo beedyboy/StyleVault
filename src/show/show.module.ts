d0656cf5e239b1eb1011ef4649efbf9a32991845
beedyboy <boladebode@gmail.com>
2023-07-12 16:54:12 +0100

    chore: add service and configure typeorm to see show entity

--- src/show/show.module.ts
+++ src/show/show.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ShowService } from './show.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Show } from 'src/entities/show.entityt';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([Show])],
+  providers: [ShowService],
+})
 export class ShowModule {}
