a6ab5d7ddf524dc564bd09eed1e04b3f58752f6a
beedyboy <boladebode@gmail.com>
2023-07-13 09:16:11 +0100

    chore: add validation pipe for dtos

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 const port = process.env.PORT || 8000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(port, () => {
     Logger.log(`Server listening at ${port}`);
   });
